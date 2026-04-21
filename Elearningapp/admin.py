e2d29208b481e435b8ae01cbf9ebd452f41db28a
sudiprakshit05 <sudiprakshit05@gmail.com>
2026-04-21 21:37:52 +0530

    Added new feature

--- Elearningapp/admin.py
+++ Elearningapp/admin.py
@@ -3,4 +3,7 @@ from Elearningapp.models import *
 admin.site.register(Elearningadmin)
 admin.site.register(coursetype)
 admin.site.register(course)
+admin.site.register(teacher)
+admin.site.register(headlines)
+admin.site.register(elearning_users)
 # Register your models here.
--- Elearningapp/models.py
+++ Elearningapp/models.py
@@ -22,5 +22,33 @@ class course(models.Model):
     coursetypes=models.CharField(max_length=50,default='')
     name=models.CharField(max_length=50)
     file=models.ImageField(upload_to='image',default='')
+    price=models.IntegerField(default=0)
+    def _str_(self):
+        return self.name
+class teacher(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=100)
+    phone=models.IntegerField(default=0)
+    password=models.CharField(max_length=50)   
+    file=models.ImageField(upload_to='image',default='')    
+    def _str_(self):
+        return self.name        
+
+class headlines(models.Model):
+    heading1=models.CharField(max_length=50)
+    heading2=models.CharField(max_length=50)
+    heading3=models.CharField(max_length=50)
+    file=models.ImageField(upload_to='image',default='')    
+    def _str_(self):
+        return self.heading1        
+
+class elearning_users(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=100)
+    phone=models.IntegerField(default=0)
+    password=models.CharField(max_length=50)
+    confirm_password=models.CharField(max_length=50)   
+    school_college=models.CharField(max_length=100,default='')
+    address=models.CharField(max_length=100,default='')
     def _str_(self):
         return self.name
